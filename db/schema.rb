2b2903d378a331014720bbc470a9b9539017a78f
Abdullah Numan <anewman15@hotmail.com>
2020-09-26 17:06:41 +0600

    Change post.body type to text

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_25_123609) do
+ActiveRecord::Schema.define(version: 2020_09_26_082246) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.string "body"
+    t.text "body"
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
