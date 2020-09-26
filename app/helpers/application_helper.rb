630ea5f28cd91556a4a4c8989a9c2721a7bdb9e0
Abdullah Numan <anewman15@hotmail.com>
2020-09-26 14:16:12 +0600

    Create view helpers for showing name and gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def display_gravatar_image(post)
+    gravatar_image_tag(post.user.email, size: 32, alt: "#{post.user.fname} #{post.user.lname}") if post.user && user_signed_in?
+  end
+
+  def display_taunter_name(post)
+    post.user && user_signed_in? ? "#{post.user.fname} #{post.user.lname} wrote:" : "Someone wrote:"
+  end
 end
